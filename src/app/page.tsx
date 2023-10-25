fd0d2add0825a405f70ffcf429508ad304a0ff46
nachovigilante <nachovigilante@gmail.com>
2023-10-25 16:44:56 -0300

    feat: added index and main css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <div className="carrousel">
+                <div className="slider-container">
+                    <div className="slide">
+                        <img src="./assets/slider/slide1.jpeg" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide2.jpeg" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide3.jpeg" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide4.png" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide5.jpg" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide6.png" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="./assets/slider/slide7.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="carrousel-overlay">
+                <div className="carrousel-controls">
+                    <button id="prev">{`<`}</button>
+                    <div id="bullets"></div>
+                    <button id="next">{'>'}</button>
+                </div>
+            </div>
+            <section className="novedades">
+                <div className="w-full max-w-[1280px]">
+                    <h2>Novedades</h2>
+                    <div className="novedades-container">
+                        <div className="novedad">
+                            <img src="./assets/digital.png" alt="" />
+                            <h3>Descubrí el Reino Digital</h3>
+                            <p>
+                                De la mano de Ivo recorre los sinuosos caminos
+                                del Reino Digital. Desde intentar conectar un
+                                módulo bluethooth por 2 horas hasta que te
+                                enterás que está quemado, hasta conectar una
+                                ESP-45 al WiFi de proyecto. Todo eso y mucho más
+                                poder hacer en este reino.
+                            </p>
+                        </div>
+                        <div className="novedad">
+                            <img src="./assets/mep.png" alt="" />
+                            <h3>¡Compartí entre 3!</h3>
+                            <p>
+                                Una <b>M</b>ilanesa gigante con 3 <b>E</b>
+                                mpanadas y <b>P</b>apas. El combo que no puede
+                                faltar en la pecera. Te podes llevar 2 combos
+                                por solo $8.500.
+                            </p>
+                        </div>
+                        <div className="novedad">
+                            <img src="" alt="" />
+                            <h3>
+                                ¡Los que aman grandes y chicos llegaron a BK!
+                            </h3>
+                            <p>
+                                8 increíbles juguetes de Warner acompañan
+                                nuestras cajitas mágicas para que vos o tus
+                                peques puedan disfrutarlos como nadie.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="footer">
+                <div className="container">
+                    <div className="top">
+                        <div className="footer-logo">BURGER TIC.</div>
+                        <div className="sitemap">
+                            <div className="sitemap-list">
+                                <h3>Info BT</h3>
+                                <ul>
+                                    <li>
+                                        <a href="./menu.html">Menú</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Sobre nosotros
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Contacto
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="sitemap-list">
+                                <h3>Otros sitios</h3>
+                                <ul>
+                                    <li>
+                                        <a href="https://cheatsheets-nachovigilante.vercel.app/">
+                                            TIC Cheatsheets
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.ort.edu.ar/ticexperience/">
+                                            TIC Experience
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            TIC Website
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="sitemap-list">
+                                <h3>My BT</h3>
+                                <ul>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Registrarse
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Iniciar sesión
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Mi cuenta
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Política de privacidad
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                            Defensa de las y los consumidores
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <p>
+                            TM © 2023 Burger Tic Corporation. Todos los
+                            derechos reservados.
+                        </p>
+                        <div className="social-media">
+                            <a href="">
+                                <img src="./assets/facebook.png" alt="" />
+                            </a>
+                            <a href="">
+                                <img src="./assets/instagram.png" alt="" />
+                            </a>
+                            <a href="">
+                                <img src="./assets/youtube.png" alt="" />
+                            </a>
+                            <a href="">
+                                <img src="./assets/twitter.png" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
