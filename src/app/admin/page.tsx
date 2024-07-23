db41903bf9afedbac2c186b3212c8b339d3a299a
nachovigilante <nachovigilante@gmail.com>
2024-07-23 02:18:58 -0300

    feat: started with the admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,24 @@
 import Link from 'next/link';
 import useAuth from '~/hooks/useAuth';
 
-const Admin = () => {
+function AdminLink({
+    href,
+    title,
+    image,
+}: {
+    href: string;
+    title: string;
+    image: string;
+}) {
+    return (
+        <Link className="flex flex-col box p-5 gap-2.5" href={href}>
+            <img src={image} alt="platos" width="300px" />
+            <h2 className="text-2xl">{title}</h2>
+        </Link>
+    );
+}
+
+function Admin() {
     const { user } = useAuth();
 
     if (!user.admin) {
@@ -16,22 +33,27 @@ const Admin = () => {
     }
 
     return (
-        <div className="container pt-10 flex flex-col items-center pb-10">
+        <div className="container pt-10 flex flex-col items-center pb-10 min-h-[600px]">
             <h2 className="text-3xl w-full">Admin</h2>
-            <Link className="" href={`/admin/platos`}>
-                <div className="flex flex-col">
-                    <img src="/assets/items/1.png" alt="platos" width="300px" />
-                    <p>Administrar platos</p>
-                </div>
-            </Link>
-            <Link className="" href={`/admin/usuarios`}>
-                Administrar usuarios
-            </Link>
-            <Link className="" href={`/admin/pedidos`}>
-                Administrar pedidos
-            </Link>
+            <div className="flex gap-5 pt-20">
+                <AdminLink
+                    href="/admin/platos"
+                    title="Administrar platos"
+                    image="/assets/items/1.png"
+                />
+                <AdminLink
+                    href="/admin/usuarios"
+                    title="Administrar usuarios"
+                    image="/assets/items/2.png"
+                />
+                <AdminLink
+                    href="/admin/pedidos"
+                    title="Administrar pedidos"
+                    image="/assets/items/3.png"
+                />
+            </div>
         </div>
     );
-};
+}
 
 export default Admin;
--- src/app/admin/platos/page.tsx
+++ src/app/admin/platos/page.tsx
@@ -1,6 +1,6 @@
 const Admin = () => {
     return (
-        <div className="container pt-10 flex flex-col items-center pb-10">
+        <div className="container pt-10 flex flex-col items-center pb-10 min-h-[600px]">
             <h2 className="text-3xl w-full">Administrador de platos</h2>
             
         </div>
